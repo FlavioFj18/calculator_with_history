60d61c357d305e243cd0f0e4ad672436aef993fe
FlavioFj18 <fuxiflavio86@gmail.com>
2025-12-05 15:18:03 +0100

    JavaScript added to the page
    Now the calculator retrieves the entered values and performs calculations with them

---
+++ app.js
@@ -0,0 +1,47 @@
+var display = document.getElementById("display");
+var buttons = document.querySelectorAll(".btn");
+var clean = document.getElementById("clean");
+var backSpace = document.getElementById('backspace');
+var historyList = document.getElementById("history-list");
+var currentText = '';
+var result = '';
+
+
+//console.log(buttons[4].textContent);
+
+clean.addEventListener('click', () => {
+  currentText = '';
+  display.value = '';
+})
+
+backSpace.addEventListener('click', () => {
+  currentText = currentText.slice(0, currentText.length - 1);
+  display.value = currentText;
+})
+
+buttons.forEach((e) => {
+  e.addEventListener('click', () => {
+    if (currentText === '' && e.className != 'btn number')
+    {
+      display.value = "Error";
+      console.log('Error');
+    }
+    else if (e.textContent === "=")
+    {
+      try {
+        result = eval(currentText);
+        console.log(result);
+        currentText = result;
+        display.value = result;
+      } catch (error) {
+        console.log('Error:', + error);
+      }
+    }
+    else
+    {
+      currentText += e.textContent;
+      display.value = currentText
+      console.log(currentText);
+    }
+  })
+});
