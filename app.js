eade23015a0c2ce8a442f5a76dabd9ad61bbb41b
FlavioFj18 <fuxiflavio86@gmail.com>
2025-12-06 22:37:23 +0100

    Updating script
    Replacing the eval function with manually created functions for security reasons

--- app.js
+++ app.js
@@ -1,63 +1,93 @@
 var display = document.getElementById("display");
 var buttons = document.querySelectorAll(".btn");
 var clean = document.getElementById("clean");
-var backSpace = document.getElementById('backspace');
+var backSpace = document.getElementById("backspace");
 var historyList = document.getElementById("history-list");
 var calcHistory = [];
-var currentText = '';
-var result = '';
+var currentText = "";
+var result = "";
 
+clean.addEventListener("click", () => {
+  currentText = "";
+  display.value = "";
+});
 
-clean.addEventListener('click', () => {
-  currentText = '';
-  display.value = '';
-})
-
-backSpace.addEventListener('click', () => {
+backSpace.addEventListener("click", () => {
   currentText = currentText.slice(0, currentText.length - 1);
   display.value = currentText;
-})
+});
 
 buttons.forEach((e) => {
-  e.addEventListener('click', () => {
-    if (currentText === '' && e.className != 'btn number')
-    {
+  e.addEventListener("click", () => {
+    if (currentText === "" && e.className != "btn number") {
       display.value = "Error";
-      currentText = '';
-      console.log('There are no values');
-      alert('Invalid expression!');
-      return ;
-    }
-    else if (e.textContent === "=")
-    {
+      currentText = "";
+      console.log("There are no values");
+      alert("Invalid expression!");
+      return;
+    } else if (e.textContent === "=") {
       try {
-        result = eval(currentText);
-        updateHistory(currentText + '=' + result);
+        result = calculator(currentText, getIndexOfOperator(currentText), getResult);
+        updateHistory(currentText + "=" + result);
         currentText = result;
         display.value = result;
       } catch (error) {
-        currentText = '';
+        currentText = "";
         display.value = "Error";
         alert(error);
-        return ;
+        return;
       }
-    }
-    else
-    {
+    } else {
       currentText += e.textContent;
-      display.value = currentText
+      display.value = currentText;
     }
-  })
+  });
 });
 
 function updateHistory(expression) {
   calcHistory.push(expression);
-  historyList.innerHTML = '';
+  historyList.innerHTML = "";
   let oldItems = calcHistory.slice().reverse();
   for (let i = 0; i < oldItems.length; i++) {
-    let li = document.createElement('li');
+    let li = document.createElement("li");
     li.textContent = oldItems[i];
     historyList.appendChild(li);
   }
 }
 
+function getResult(str, op) {
+  let n = 0;
+  if (op == "+") {
+    n = Number(str[0]) + Number(str[1]);
+  }
+  if (op == "-") {
+    n = Number(str[0]) - Number(str[1]);
+  }
+  if (op == "*") {
+    n = Number(str[0]) * Number(str[1]);
+  }
+  if (op == "/") {
+    n = Number(str[0]) / Number(str[1]);
+  }
+  if (op == "%") {
+    n = Number(str[0]) % Number(str[1]);
+  }
+  return n;
+}
+
+function getIndexOfOperator(str) {
+  let p = ["+", "-", "*", "/", "%"];
+  for (let i = 0; i < p.length; i++) {
+    let index = str.indexOf(p[i]);
+    if (index != -1) {
+      return index;
+    }
+  }
+}
+
+function calculator(str, p, getResult) {
+  let c = str[p];
+  let expression = str.split(c);
+  let result = getResult(expression, c);
+  return result;
+}
